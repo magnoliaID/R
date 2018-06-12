a5caead118d947e0baf39b85d44542f8362d9905
Giri Adi N <giriadn97@gmail.com>
2018-06-12 22:05:56 +0700

    Update klasifikasi Hoax.R

--- klasifikasi Hoax.R
+++ klasifikasi Hoax.R
@@ -3,26 +3,26 @@ library(tm)
 library(SnowballC)
 library(arm)
 # Training data.
-data <- c('Nasa : ada belasan ribu Satelit di angkasa',
-          'Bumi berbentuk bulat ',
-          'Antartika adalah wilayah kecil',
-          'Amstrong adalah manusia pertama yang mendarat di bulan',
-          'Roket mampu menembus atmosfer bumi',
-          'Game PUBG merupakan game balapan',
-          'Mobile Legend adalah game dengan grafik terbaik',
-          'Manusia mampu menciptakan mesin waktu',
-          'Kucing terlahir dengan warna pink',
-          'Burung merpati adalah hewan mamalia',
-          'Tidak ada satupun satelit di angkasa',
-          'Bentuk bumi adalah datar',
-          'Antartika adalah wilayah es besar yang mengelilingi bumi',
-          'Tidak ada manusia yang mendarat di bulan',
-          'Roket tidak mampu menembus atmosfer bumi',
-          'PUBG merupakan game survival',
-          'Grafik mobile legend jelek',
-          'Manusia tidak bisa membuat mesin waktu',
-          'kucing terlahir dengan warna coklat,hitam,putih,oranye dan kombinasi',
-          'Burung merpati adalah hewan aves')
+data <- c('Viral Nomor IMEI Ponsel Bisa Disadap, Kemkominfo: Itu Hoax',
+          'Kemkominfo Siapkan Satelit untuk Perkuat Sinyal Daerah Terpencil',
+          'Kemkominfo Uji Algoritma Bigo untuk Berburu Konten Pornografi',
+          'Mesin Pengais Konten Negatif Kemkominfo Batasi Gerak Teroris di Dunia Maya',
+          'Kemkominfo Gandeng Facebook Cs Babat Ratusan Akun Radikalisme',
+          'INASGOG Gandeng Kemkominfo Amankan Asian Games 2018 dari Hacker',
+          'Pemerintah Terus Berupaya Antisipasi Penyebaran Hoax',
+          'Kemkominfo: Satu NIK Bisa Registrasikan Banyak Nomor',
+          'Kemkominfo Siap Tangkis Lebih Banyak Konten Porno di Internet',
+          'Kemkominfo: Penyebar Konten Porno Running Text Solo Tertangkap',
+          'Kemkominfo sadap ponsel lewat IMEI',
+          'Sinyal satelit tidak menjangkau daerah terpencil',
+          'Bigo adalah situs porno',
+          'Teroris terbantu oleh konten kemkomifo',
+          'Kemkominfo bantu facebook membuat akun radikal',
+          'Asian games di hack',
+          'Pemerintah menyebarkan berita hoax',
+          'Satu NIK hanya bisa registrasi sekali',
+          'Banyak konten porno dilegalkan oleh kemkominfo',
+          'Penyebar konten porno dibebaskan')
 corpus <- VCorpus(VectorSource(data))
 
 # Create a document term matrix.
@@ -43,16 +43,16 @@ fit <- train(y ~ ., data = train, method = 'bayesglm')
 predict(fit, newdata = train)
 
 # Test data.
-data2 <- c('Peneliti buktikan tidak ada satupun satelit di angkasa',
-           'Bentuk bumi yang kita tempati adalah datar',
-           'Antartika wilayah es yang mengelilingi bumi',
-           'Manusia tidak mungkin mendarat di bulan',
-           'Roket tidak akan mampu menembus atmosfer bumi',
-           'PUBG adalah game survival terlaris',
-           'Game mobile legend memiliki grafik jelek',
-           'Tidak mungkin mesin waktu dapat dibuat',
-           'seekor kucing persia berwarnan hitam menjadi viral',
-           'Burung merupakan kelompok aves')
+data2 <- c('Sadap ponsel lewat IMEI adalah hoax',
+           'Sinyal satelit tidak akan mencapai wilayah terpencil',
+           'Algoritma bigo diuji oleh kemkominfo',
+           'Teroris berterimakasih pada kemkominfo',
+           'Kemkominfo dan facebook berantas akun radikal',
+           'Asian games diserang hacker',
+           'Pemerintah akan antisipasi berita hoax',
+           'satu NIK sekali registrasi',
+           'Kemkominfo tangkis konten porno',
+           'Kemkominfo bebaskan konten porno')
 corpus <- VCorpus(VectorSource(data2))
 tdm <- DocumentTermMatrix(corpus, control = list(dictionary = Terms(tdm), removePunctuation = TRUE, stopwords = TRUE, stemming = TRUE, removeNumbers = TRUE))
 test <- as.matrix(tdm)
